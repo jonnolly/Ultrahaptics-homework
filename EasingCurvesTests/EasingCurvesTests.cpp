c4a732948ec8fbf92a5211f44bad21e97daf4079
John Connolly <jc140593@renishaw.com>
2018-05-31 01:06:00 +0100

    Added empty test project & tests

---
+++ EasingCurvesTests/EasingCurvesTests.cpp
@@ -0,0 +1,49 @@
+// EasingCurvesTests.cpp : Defines the entry point for the console application.
+//
+
+// unfortunately I didn't download GMocks on the plane, which is what I would have normally have used.
+// in replacement, I've got these home-made tests
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
+void GivenUserInputIsInvalid_WhenApplicationRuns_LineIsIgnoredAndApplicationContinues()
+{
+	// Arrange
+
+	// Act
+
+	// Assert
+}
+
+void GivenUserInputIsValidCurveLine_WhenApplicationRuns_LineIsPrinted()
+{
+	// Arrange
+
+	// Act
+
+	// Assert
+}
+
+void GivenUserInputIsValidCurveLine_WhenUserInputsValidTimeValues_TimeValuesAreEvaluated()
+{
+	// Arrange
+
+	// Act
+
+	// Assert
+}
+
+void GivenNoCurveLineHasBeenPreviouslyInputted_WhenUserInputsValidTimeValues_TheLineIsIgnored()
+{
+	// Arrange
+
+	// Act
+
+	// Assert
+}
